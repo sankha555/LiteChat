19941c176a4cf03897e18bb7ad638dce72054c2d
sankha555 <f20190029@pilani.bits-pilani.ac.in>
2020-06-22 12:12:15 +0530

    scrolling bugs

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,7 @@ const messageForm = document.getElementById('send-form')
 const messageInput = document.getElementById('btn-input')
 const messageCallout = document.getElementById('message-container')
 const chatbox = document.getElementById('chatbox')
-const status = document.getElementById('status').innerText
+var status = document.getElementById('status').innerText
 const sender_email = document.getElementById('sender_email').innerText
 const recipient_email = document.getElementById('recipient_email').innerText
 
@@ -31,7 +31,7 @@ socket.on('is-typing', (data) => {
 })
 
 socket.on('chat-message', data => {
-    appendMessage(data, true)
+    appendMessage(data.message, data.image, true)
     if (data.alert) {
         // in next verion :)
     }
@@ -49,30 +49,30 @@ messageForm.addEventListener('submit', e => {
         timestamp: new Date(),
     }
     data.seen = ((status === "Active") ? true : false)
-    appendMessage(data, false)
+    appendMessage(data, null, false)
 })
 
-function appendMessage(message, received) {
+function appendMessage(message, image, received) {
     if (received) {
-        const receive_row = document.createElement('div')
+        let receive_row = document.createElement('div')
         receive_row.setAttribute("class", "row msg_container base_receive")
-        const receive_col = document.createElement('div')
+        let receive_col = document.createElement('div')
         receive_col.setAttribute("class", "col-xs-10 col-md-10")
-        const sender_img_holder = document.createElement('div')
+        let sender_img_holder = document.createElement('div')
         sender_img_holder.setAttribute("class", "col-md-2 col-xs-2 avatar")
-        const sender_img = document.createElement('img')
+        let sender_img = document.createElement('img')
         sender_img.setAttribute("class", "img-responsive")
-        sender_img.setAttribute("src", data.image)
+        sender_img.setAttribute("src", image)
 
         sender_img_holder.appendChild(sender_img)
 
-        const message_text = document.createElement('div')
+        let message_text = document.createElement('div')
         message_text.setAttribute("class", "messages msg_receive")
-        const msg_content = document.createElement('p')
-        const msg_time = document.createElement('p')
+        let msg_content = document.createElement('p')
+        let msg_time = document.createElement('p')
 
-        msg_time.inputHTML = '<i>' + data.message.timestamp + '</i>'
-        msg_content.innerText = data.message.content
+        msg_time.inputHTML = '<i>' + message.timestamp + '</i>'
+        msg_content.innerText = message.content
         message_text.appendChild(msg_content)
         message_text.appendChild(msg_time)
         receive_col.appendChild(message_text)
@@ -85,15 +85,15 @@ function appendMessage(message, received) {
 
         const shortMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-        const sent_row = document.createElement('div')
+        let sent_row = document.createElement('div')
         sent_row.setAttribute("class", "row msg_container base_sent")
-        const sent_col = document.createElement('div')
+        let sent_col = document.createElement('div')
         sent_col.setAttribute("class", "col-xs-10 col-md-10")
-        const message_text = document.createElement('div')
+        let message_text = document.createElement('div')
         message_text.setAttribute("class", "messages msg_sent")
         message.seen ? message_text.setAttribute("style", "background-color: lightgreen;") : message_text.setAttribute("style", "background-color: lightblue;")
-        const msg_content = document.createElement('p')
-        const msg_time = document.createElement('i')
+        let msg_content = document.createElement('p')
+        let msg_time = document.createElement('i')
 
         msg_time.innerText = message.timestamp.getDate() + " " + shortMonths[message.timestamp.getMonth()] + " " + (message.timestamp.getFullYear()) % 100 + " - " + (message.timestamp.getHours())%12 + ":" + ((message.timestamp.getMinutes() < 10) ? "0"+message.timestamp.getMinutes() : message.timestamp.getMinutes()) + " " + ((message.timestamp.getHours() > 12) ? "pm" : "am") 
         msg_content.innerText = message.content
