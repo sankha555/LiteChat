a3f4ed637dc2a5f8657555e509766bad38686848
sankha555 <f20190029@pilani.bits-pilani.ac.in>
2020-06-22 12:31:59 +0530

    scrolling bugs

--- public/js/script.js
+++ public/js/script.js
@@ -14,7 +14,7 @@ socket.emit('user-connected', {'room': document.getElementById('chat_id').innerT
 
 socket.on('recipient-is-in', () => {
     console.log('recipient in')
-    status.innerText = "Active"
+    status.innerHTML = "Active"
     var x = document.getElementsByClassName('messages msg_sent')
     for(var i = 0; i < x.length; i++){
         x[i].setAttribute("style", "background-color: lightgreen;")
@@ -23,13 +23,13 @@ socket.on('recipient-is-in', () => {
 
 socket.on('recipient-out', () => {
     console.log('recipient in')
-    status.innerText = ""
+    status.innerHTML = ""
 })
 
 socket.on('is-typing', (data) => {
     console.log('here in typing')
     if (data.true)
-        status.innerText = "typing..."
+        status.innerHTML = "typing..."
 })
 
 socket.on('chat-message', data => {
@@ -50,7 +50,7 @@ messageForm.addEventListener('submit', e => {
         image: null,
         timestamp: new Date(),
     }
-    data.seen = ((status === "Active") ? true : false)
+    data.seen = ((status.innerHTML === "Active") ? true : false)
     appendMessage(data, null, false)
 })
 
@@ -94,7 +94,7 @@ function appendMessage(message, image, received) {
         sent_col.setAttribute("class", "col-xs-10 col-md-10")
         let message_text = document.createElement('div')
         message_text.setAttribute("class", "messages msg_sent")
-        (status.innerText === "Active") ? message_text.setAttribute("style", "background-color: lightgreen;") : message_text.setAttribute("style", "background-color: lightblue;")
+        (data.seen) ? message_text.setAttribute("style", "background-color: lightgreen;") : message_text.setAttribute("style", "background-color: lightblue;")
         let msg_content = document.createElement('p')
         let msg_time = document.createElement('i')
 
